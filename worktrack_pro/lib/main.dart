227bc87089198f584f234cec3a9f63c1a2c9a3af
Omor Faruqe <omor.software.dev@gmail.com>
2026-07-19 16:10:40 +0600

    Create authentication module

--- worktrack_pro/lib/main.dart
+++ worktrack_pro/lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
   runApp(const MyApp());
 }
 
@@ -9,12 +20,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'WorkTrack Pro',
       home: Scaffold(
         body: Center(
-          child: Text('WorkTrack Pro'),
+          child: Text("WorkTrack Pro"),
         ),
       ),
     );
